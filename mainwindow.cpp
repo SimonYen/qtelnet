2c5294d70aaa24439aaf24b5b9fa56884ca7b6b0
深川 <simonyen@foxmail.com>
2025-04-09 16:39:35 +0800

    添加图标，连接设置页面完成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,66 @@
 #include "mainwindow.h"
+#include <QRegularExpressionValidator>
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , m_socketType(SocketType::TCP_CLIENT)
 {
     ui->setupUi(this);
+    //设置输入限制
+    settingInputValidation();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+//当套接字类型选择框有所改变的时候
+void MainWindow::on_socketTypeCombox_currentIndexChanged(int index)
+{
+    //切换成员变量
+    m_socketType = static_cast<SocketType>(index);
+    //输出出来
+    ui->statusbar->showMessage("工具切换到：" + SocketTypeToString(index));
+}
 
+void MainWindow::settingInputValidation()
+{
+    // 定义正则表达式：允许0或非零开头的1-65535
+    QRegularExpression portRegExp(
+        "^(0|[1-9]\\d{0,3}|[1-5]\\d{4}|6[0-4]\\d{3}|65[0-4]\\d{2}|655[0-2]\\d|6553[0-5])$");
+    // 创建端口验证器
+    QRegularExpressionValidator *portValidator = new QRegularExpressionValidator(portRegExp, this);
+    ui->portLineEdit->setValidator(portValidator);
+}
+//开始连接
+void MainWindow::on_startConnectButton_clicked()
+{
+    //获取地址和端口
+    auto host = ui->hostLineEdit->text();
+    auto port = ui->portLineEdit->text().toInt();
+    //如果地址为空，那就采用本地loop地址
+    if (host.isEmpty())
+        host = "127.0.0.1";
+    ui->statusbar->showMessage(QString("开始连接，host: %1，port: %2").arg(host).arg(port));
+    m_address = {host, port};
+    //初始化对象
+
+    //更改按钮属性
+    ui->startConnectButton->setText("连接中");
+    ui->startConnectButton->setEnabled(false);
+    ui->closeConnectButton->setEnabled(true);
+}
+//关闭连接
+void MainWindow::on_closeConnectButton_clicked()
+{
+    //关闭对象
+
+    ui->statusbar->showMessage("连接已关闭");
+    m_address = {"", 0};
+    //还原按钮属性
+    ui->startConnectButton->setText("开始连接");
+    ui->startConnectButton->setEnabled(true);
+    ui->closeConnectButton->setEnabled(false);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "GlobalVariable.hpp"
+
 #include <QMainWindow>
+#include <QPair>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,22 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_socketTypeCombox_currentIndexChanged(int index);
+
+    void on_startConnectButton_clicked();
+
+    void on_closeConnectButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    //当前所处的模式（工具所选择的套接字类型）
+    SocketType m_socketType;
+    //IP地址
+    QPair<QString, int> m_address;
+
+private:
+    //设置输入验证
+    void settingInputValidation();
 };
 #endif // MAINWINDOW_H
